564eeded22946a83448d0cff947baf189d6b11e8
Andrew Hodgkinson <ahodgkin@rowing.org.uk>
2025-02-06 14:13:27 +1300

    Logging improvements

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,18 @@ module Radiant
       end
     end
 
+    # Custom format and rotation for logs.
+    #
+    class CustomLoggerFormatter < Logger::Formatter
+      def call(severity, time, progname, msg)
+        # https://github.com/ruby/logger/blob/master/lib/logger/formatter.rb#L16
+        "#{severity[0]} #{time.iso8601(2)}: #{msg2str(msg)}\n"
+      end
+    end
+
+    config.logger           = Logger.new(Rails.root.join('log', "#{Rails.env}.log"), 'daily')
+    config.logger.formatter = CustomLoggerFormatter.new
+
     # Add the shared ROOL view components.
     #
     shared_views_path = if ENV['SHARED_FILES_PATH'].blank?
